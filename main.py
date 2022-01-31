1c5015a48aaa88c4bb0514fb79584c9748b977c0
francescoreconditi <francesco.reconditi@gmail.com>
2022-01-31 18:11:43 +0100

    .

--- main.py
+++ main.py
@@ -3,17 +3,23 @@
 #
 
 import os
+import pathlib
 import time
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
+from py.xml_distinta import check_xml_distinta
+
 #
 # fff Costanti
 #
 
+STRUCT_XML: str = "_05806_"
+
 CHECK_PATH = "c:\\temp\\origine"
-DEST_PATH = "c:\\temp\\destinazione"
+XML_PATH = "c:\\temp\\origine\\xml"
+GARBAGE_PATH = "c:\\temp\\origine\\garbage"
 
 #
 # fff Flusso
@@ -27,15 +33,32 @@ class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
 
         for filename in os.listdir(CHECK_PATH):
-
-            # ! File origine
+            # ! Path completo
             src = os.path.join(CHECK_PATH, filename)
 
-            # ! File Destinazione
-            new_destination = os.path.join(DEST_PATH, filename)
+            # ! Scarto le directory
+            if pathlib.Path(src).is_dir():
+                continue
 
-            # ! lo muovo
-            os.rename(src, new_destination)
+            # ! Controllo se e' un ".xml", e se contiene la stringa STRUCT_XML
+            file_extension = pathlib.Path(filename).suffix
+
+            if (file_extension.upper() == ".XML") and (STRUCT_XML in filename):
+
+                # ! Determinazione file origine e di destinazione
+                dest = os.path.join(XML_PATH, filename)
+
+                # ! lo muovo
+                os.rename(src, dest)
+
+                # ! Lo "analizzo"
+                check_xml_distinta(dest)
+            else:
+                # ! Lo metto da un' altra parte
+                dest = os.path.join(GARBAGE_PATH, filename)
+
+                # ! lo muovo
+                os.rename(src, dest)
 
 
 if __name__ == "__main__":
